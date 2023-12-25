16df5e2877238dc18b79b8d9314b8d5b6bab1c45
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2023-12-25 13:51:04 +0400

    index.html add

--- cherez/core/urls.py
+++ cherez/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.index_view, name="index_view"),
+    path("", views.index_view, name="index_view"),
     # path("contact-us/", views.contact_us_view, name="contact_us_view"),
     # path("about-us/", views.about_us_view, name="about_us_view"),
 ]
